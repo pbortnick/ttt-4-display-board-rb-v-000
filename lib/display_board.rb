afb587e6b5fa4ffc4f4994b405ecd54d525ec138
Pamela Bortnick <pbortnick@gmail.com>
2018-07-11 22:48:17 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,4 +7,4 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-board = [" "," "," ","O","X"," "," "," "," "]
+my_board = [" "," "," ","O","X"," "," "," "," "]
